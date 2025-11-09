18d39fd2de13aeaf734f68a266593e920fee6ce5
Madaminjon Madazimov <madazimovmadaminjonn@gmail.com>
2025-11-09 17:35:41 +0600

    React Mold

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='*' element={<NotFound/>} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react'
+
+const AppCotext = createContext({})
+
+export default AppCotext
